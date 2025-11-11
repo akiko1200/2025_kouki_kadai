fa7b54ec75f6dc89278a4fe9304f99e428990fba
akiko1200 <acco.chan.1200.s@gmail.com>
2025-11-11 12:23:01 +0900

    カードを追加

--- 1年後期課題/1年後期課題/Form1.cs
+++ 1年後期課題/1年後期課題/Form1.cs
@@ -12,9 +12,67 @@ namespace _1年後期課題
 {
     public partial class Form1 : Form
     {
+        /// <summary>
+        /// カードの横幅
+        /// </summary>
+        const int CARD_SIZE_X = 100;
+
+        /// <summary>
+        /// カードの縦幅
+        /// </summary>
+        const int CARD_SIZE_Y = 100;
+
+        /// <summary>
+        /// カードが横に何個並ぶか
+        /// </summary>
+        const int BOARD_SIZE_X = 3;
+
+        /// <summary>
+        /// カードが縦に何個並ぶか
+        /// </summary>
+        const int BOARD_SIZE_Y = 2;
+
+        /// <summary>
+        /// TestCardの二次元配列
+        /// </summary>
+        private TestCard[,] _cardArray;
+
+
         public Form1()
         {
             InitializeComponent();
+
+            // _cardArrayの初期化
+            _cardArray = new TestCard[BOARD_SIZE_Y, BOARD_SIZE_X];
+
+            for (int i = 0; i < BOARD_SIZE_X; i++)
+            {
+                for (int j = 0; j < BOARD_SIZE_Y; j++)
+                {
+                    // インスタンスの作成
+                    TestCard testCard =
+                        new TestCard(
+                            this,
+                            i, j,
+                            new Size(CARD_SIZE_X, CARD_SIZE_Y),
+                            "",
+                            BOARD_SIZE_X, BOARD_SIZE_Y);
+
+                    // 配列にカードの参照を追加
+                    _cardArray[j, i] = testCard;
+
+                    // コントロールにカードを追加
+                    Controls.Add(testCard);
+                }
+            }
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ 1年後期課題/1年後期課題/TestCard.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1年後期課題
+{
+    internal class TestCard : Button
+    {
+        /// <summary>Form1の参照</summary>
+        private Form1 _form1;
+
+        /// <summary>横位置</summary>
+        private int _x;
+
+        /// <summary>縦位置</summary>
+        private int _y;
+
+        /// <summary>ボタンの列数</summary>
+        private int _board_size_x;
+
+        /// <summary>ボタンの行数</summary>
+        private int _board_size_y;
+
+        public TestCard(Form1 form1, int x, int y, Size size,
+            string text, int board_size_x, int board_size_y)
+        {
+            // Form1の参照を保管
+            _form1 = form1;
+
+            // 横位置を保管
+            _x = x;
+
+            // 縦位置を保管
+            _y = y;
+
+            // カードの位置を設定
+            Location = new Point(x * size.Width, y * size.Height);
+            // カードの大きさを設定
+            Size = size;
+            // カード内のテキストを設定
+            Text = text;
+
+            // カードの列数を保管
+            _board_size_x = board_size_x;
+
+            // カードの行数を保管
+            _board_size_y = board_size_y;
+
+
+        }
+    }
+}
